4f4f756a921069f7175312add28f8917b48a7ef0
nguyenphuong021100@gmail.com <nguyenphuong021100@gmail.com>
2021-10-21 15:35:21 +0700

    push tinh nang login remember

--- EXON_ExamManagement-master/EOS.Main/Module/Forms/FrmLogin.Designer.cs
+++ EXON_ExamManagement-master/EOS.Main/Module/Forms/FrmLogin.Designer.cs
@@ -30,13 +30,12 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmLogin));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.txtUsername = new System.Windows.Forms.TextBox();
             this.txtPassword = new System.Windows.Forms.TextBox();
             this.ceLuuMatKhau = new System.Windows.Forms.CheckBox();
             this.ceHienMatKhau = new System.Windows.Forms.CheckBox();
             this.btnDangNhap = new System.Windows.Forms.Button();
             this.btnThoat = new System.Windows.Forms.Button();
-            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.cbbUsername = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -50,16 +49,6 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // txtUsername
-            // 
-            this.txtUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtUsername.Location = new System.Drawing.Point(36, 207);
-            this.txtUsername.Multiline = true;
-            this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Size = new System.Drawing.Size(292, 38);
-            this.txtUsername.TabIndex = 1;
-            this.txtUsername.Text = "hant";
-            // 
             // txtPassword
             // 
             this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -67,9 +56,8 @@
             this.txtPassword.Multiline = true;
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.PasswordChar = '*';
-            this.txtPassword.Size = new System.Drawing.Size(292, 38);
+            this.txtPassword.Size = new System.Drawing.Size(292, 26);
             this.txtPassword.TabIndex = 2;
-            this.txtPassword.Text = "123456";
             // 
             // ceLuuMatKhau
             // 
@@ -116,16 +104,15 @@
             this.btnThoat.UseVisualStyleBackColor = false;
             this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
             // 
-            // linkLabel1
+            // cbbUsername
             // 
-            this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(3, 422);
-            this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(138, 13);
-            this.linkLabel1.TabIndex = 8;
-            this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "Bạn không thể đăng nhập?";
-            this.linkLabel1.Click += new System.EventHandler(this.linkLabel1_Click);
+            this.cbbUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbbUsername.FormattingEnabled = true;
+            this.cbbUsername.Location = new System.Drawing.Point(36, 219);
+            this.cbbUsername.Name = "cbbUsername";
+            this.cbbUsername.Size = new System.Drawing.Size(292, 26);
+            this.cbbUsername.TabIndex = 7;
+            this.cbbUsername.SelectedIndexChanged += new System.EventHandler(this.cbbUsername_SelectedIndexChanged);
             // 
             // FrmLogin
             // 
@@ -134,13 +121,12 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(364, 444);
-            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.cbbUsername);
             this.Controls.Add(this.btnThoat);
             this.Controls.Add(this.btnDangNhap);
             this.Controls.Add(this.ceHienMatKhau);
             this.Controls.Add(this.ceLuuMatKhau);
             this.Controls.Add(this.txtPassword);
-            this.Controls.Add(this.txtUsername);
             this.Controls.Add(this.pictureBox1);
             this.Name = "FrmLogin";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -152,12 +138,11 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.TextBox txtUsername;
         private System.Windows.Forms.TextBox txtPassword;
         private System.Windows.Forms.CheckBox ceLuuMatKhau;
         private System.Windows.Forms.CheckBox ceHienMatKhau;
         private System.Windows.Forms.Button btnDangNhap;
         private System.Windows.Forms.Button btnThoat;
-        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.ComboBox cbbUsername;
     }
 }
--- EXON_ExamManagement-master/EOS.Main/Module/Forms/FrmLogin.cs
+++ EXON_ExamManagement-master/EOS.Main/Module/Forms/FrmLogin.cs
@@ -3,13 +3,21 @@ using EXON.Data.Services;
 using MetroFramework.Forms;
 using System;
 using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
 
 namespace EXON.Main.Module.Forms
 {
     public partial class FrmLogin : MetroForm
     {
         private StaffService _StaffService;
-
+        public class account
+        {
+            public string username { get; set; }
+            public string password { get; set; }
+        }
         public LoginStatus LoginStatus
         {
             get;
@@ -22,33 +30,127 @@ namespace EXON.Main.Module.Forms
             private set;
         }
 
+        public List<account> Readjs()
+        {
+            if (System.IO.File.Exists("account.json"))
+            {
+                FileStream fs = new FileStream("account.json", FileMode.Open);
+                StreamReader rd = new StreamReader(fs, Encoding.UTF8);
+                string jsonAccount = rd.ReadToEnd();
+                fs.Close();
+                if (jsonAccount != " ")
+                {
+                    var acc = JsonConvert.DeserializeObject<List<account>>(jsonAccount);
+                    return acc;
+                }
+                else return null;
+                
+            }
+            else
+            {
+                FileStream fs = File.Create("account.json");
+                fs.Close();
+                return null;
+            }
+           
+        }
+
+        public void Writejs(List<account> list) // luu them tai khoan vao file json
+        {
+            string json = JsonConvert.SerializeObject(list);
+            json = json.Insert(1, "\n");
+            json = json.Insert(json.Length - 1, "\n");
+            int i;
+            for (i = 0; i < json.Length; i++)
+            {
+                if (json[i].ToString() == "}" && json[i + 1].ToString() == ",")
+                {
+                    json = json.Insert(i + 2, "\n");
+                }
+            }
+            File.WriteAllText("account.json", json);
+        }
         public FrmLogin()
         {
             _StaffService = new StaffService();
             InitializeComponent();
             LoginStatus = LoginStatus.None;
             this.ControlBox = false;
-            this.txtPassword.Text = "02082015";
-            this.txtUsername.Text = "thieunv";
-            //txtUsername.Focus();
+
+            if(Readjs() != null)
+            {
+                List<account> list = Readjs();
+                if (list.Count > 0)
+                {
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        cbbUsername.Items.Add(list[i].username);
+                    }
+                }
+            }
+            
+            //this.txtPassword.Text = "02082015";
+            //this.cbbUsername.Text = "thieunv";
+            cbbUsername.Focus();
         }
 
         private void btnDangNhap_Click(object sender, EventArgs e)
         {
+
             try
             {
-                if (string.IsNullOrEmpty(txtUsername.Text) || string.IsNullOrEmpty(txtPassword.Text))
+                if (string.IsNullOrEmpty(cbbUsername.Text) || string.IsNullOrEmpty(txtPassword.Text))
                 {
                     MessageBox.Show(Properties.Resources.NotInputMessage, Properties.Resources.Error);
                     return;
                 }
                 else
                 {
-                    if (_StaffService.Login(txtUsername.Text, txtPassword.Text))
+                    if (_StaffService.Login(cbbUsername.Text, txtPassword.Text))
                     {
                         LoginStatus = LoginStatus.Login;
-                        CurrentStaffId = _StaffService.GetIdByUserPass(txtUsername.Text, txtPassword.Text);
+                        CurrentStaffId = _StaffService.GetIdByUserPass(cbbUsername.Text, txtPassword.Text);
                         this.Hide();
+                        if (ceLuuMatKhau.Checked == true)
+                        {
+
+                            account acc = new account();
+                            acc.username = cbbUsername.Text;
+                            acc.password = txtPassword.Text;
+                            
+                            if(Readjs() != null)
+                            {
+                                List<account> list = Readjs();
+                                int key = 1;
+                                for (int i = 0; i < list.Count; i++)
+                                {
+                                    if (list[i].username == acc.username && list[i].password == acc.password)
+                                    {
+                                        key = 0;
+                                        break;
+                                    }
+                                    else if(list[i].username == acc.username && list[i].password != acc.password)
+                                    {
+                                        list[i].password = acc.password;
+                                        Writejs(list);
+                                        key = 0;
+                                        break;
+                                    }
+                                }
+                                if (key == 1)
+                                {
+                                    list.Add(acc);
+                                    Writejs(list);
+                                }
+                            }
+                            else
+                            {
+                                List<account> list = new List<account>();
+                                list.Add(acc);
+                                Writejs(list);
+                            }
+                            
+                        }
                     }
                     else
                     {
@@ -78,10 +180,13 @@ namespace EXON.Main.Module.Forms
             else txtPassword.PasswordChar = '*';
         }
 
-        private void linkLabel1_Click(object sender, EventArgs e)
+        private void cbbUsername_SelectedIndexChanged(object sender, EventArgs e)
         {
-            this.linkLabel1.LinkVisited = true;
-            System.Diagnostics.Process.Start("https://web.facebook.com/help/374104332645037?helpref=faq_content");
+            List<account> list = Readjs();
+            if (cbbUsername.SelectedIndex >= 0 && cbbUsername.SelectedIndex < list.Count)
+            {
+                txtPassword.Text = list[cbbUsername.SelectedIndex].password;
+            }
         }
     }
 }
